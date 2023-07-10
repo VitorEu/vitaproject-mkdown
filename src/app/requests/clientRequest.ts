1ee6f5653dd44727a4792dd3270d7fc07473fafa
VITORPASSOS <vitoreugenio02@gmail.com>
2023-07-10 16:41:22 -0300

    api

--- src/app/requests/clientRequest.ts
+++ src/app/requests/clientRequest.ts
@@ -1,12 +1,13 @@
 import { mkdown_content } from "@prisma/client";
 import axios from "axios";
 
+
 interface IRequest {
     getContentByUUID(uuid: string): Promise<mkdown_content>
 }
 
 const getContentByUUID = async(uuid: string): Promise<mkdown_content> => {
-    return (await axios.get(`http://8be4-186-224-74-25.ngrok-free.app/editor/${uuid}`)).data
+    return (await axios.get(`https://bcf4-186-224-74-25.ngrok-free.app/editor/${uuid}`)).data
 }
 
 const clientRequest: IRequest = {
